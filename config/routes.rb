365d16ad837ba826f348995e0ac28acb352de2fa
Amanda Chen <amanda.chen2907@gmail.com>
2018-10-26 11:10:16 +1000

    added home page and configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  # get 'home/index'
+  get 'home' => 'home#index', as: 'home'
   # get '/splash' => 'discussions#splash'
   match 'search' => 'discussions#search', as: 'discussions_search', via: [:get, :post]
 
@@ -9,7 +10,8 @@ Rails.application.routes.draw do
   end
 
   # root 'discussions#splash'
-  root 'discussions#index'
+  # root 'discussions#index'
+  root 'home#index'
 
   devise_for :users, controllers: { registrations: 'registrations' }
 end
