d28f391a85c928d184a74a2c689ba47ae732620f
Amanda Chen <amanda.chen2907@gmail.com>
2018-11-05 22:47:38 +1000

    added chatbot

--- public/main.js
+++ public/main.js
@@ -1,40 +1,88 @@
-var land = document.querySelectorAll('.land');
-var cloud = document.querySelectorAll('.cloud');
 
+///Chat stuff
 
+var $messages = $('.messages-content'),
+    d, h, m,
+    i = 0;
 
-for (var i = 0; i < land.length; i++) {
-	land[i].style.transform = 'translate(' + Math.round(Math.random() * 150) + 'px, ' + Math.round(Math.random() * 150) + 'px)';
-	land[i].style.width = Math.round(Math.random() * 50) + 50 + 'px';
+$(window).load(function() {
+  $messages.mCustomScrollbar();
+  setTimeout(function() {
+    fakeMessage();
+  }, 100);
+});
+
+function updateScrollbar() {
+  $messages.mCustomScrollbar("update").mCustomScrollbar('scrollTo', 'bottom', {
+    scrollInertia: 10,
+    timeout: 0
+  });
 }
 
-for (var i = 0; i < cloud.length; i++) {
-	cloud[i].style.transform = 'translate(' + Math.round(Math.random() * 150) + 'px, ' + Math.round(Math.random() * 150) + 'px)';
-	cloud[i].style.width = Math.round(Math.random() * 25) + 25 + 'px';
+function setDate(){
+  d = new Date()
+  if (m != d.getMinutes()) {
+    m = d.getMinutes();
+    $('<div class="timestamp">' + d.getHours() + ':' + m + '</div>').appendTo($('.message:last'));
+  }
 }
 
-
-requestAnimationFrame(animate);
-
-function animate() {
-	for (var i = 0; i < land.length; i++) {
-		move(land[i]);
-		move(cloud[i]);
-	}
-
-	requestAnimationFrame(animate);
+function insertMessage() {
+  msg = $('.message-input').val();
+  if ($.trim(msg) == '') {
+    return false;
+  }
+  $('<div class="message message-personal">' + msg + '</div>').appendTo($('.mCSB_container')).addClass('new');
+  setDate();
+  $('.message-input').val(null);
+  updateScrollbar();
+  setTimeout(function() {
+    fakeMessage();
+  }, 1000 + (Math.random() * 20) * 100);
 }
 
-function move(el) {
-	var s = el.style.transform.split('(')[1].split(',');
-	var x = s[0].split('px')[0];
-	var y = s[1];
-	var w = el.style.width.split('px')[0];
+$('.message-submit').click(function() {
+  insertMessage();
+});
 
-	var nx = parseInt(x) - 1;
+$(window).on('keydown', function(e) {
+  if (e.which == 13) {
+    insertMessage();
+    return false;
+  }
+})
 
-	if (nx + parseInt(w) < -20) {
-		nx = 170;
-	}
-	el.style.transform = 'translate(' + nx + 'px, ' + y;
+var Fake = [
+  'Hey 👋 I\'m Jake. How are you today?',
+  'Nice to meet you',
+  'How are you?',
+  'Not too bad, thanks',
+  'What do you do?',
+  'That\'s awesome',
+  'Codepen is a nice place to stay',
+  'I think you\'re a nice person',
+  'Why do you think that?',
+  'Can you explain?',
+  'Anyway I\'ve gotta go now',
+  'It was a pleasure chat with you',
+  'Time to make a new codepen',
+  'Bye',
+  ':)'
+]
+
+function fakeMessage() {
+  if ($('.message-input').val() != '') {
+    return false;
+  }
+  $('<div class="message loading new"><figure class="avatar"><img src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=29a17c0d14e2ef6c46b93628fa033371&auto=format&fit=crop&w=1000&q=80" /></figure><span></span></div>').appendTo($('.mCSB_container'));
+  updateScrollbar();
+
+  setTimeout(function() {
+    $('.message.loading').remove();
+    $('<div class="message new"><figure class="avatar"><img src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=29a17c0d14e2ef6c46b93628fa033371&auto=format&fit=crop&w=1000&q=80" /></figure>' + Fake[i] + '</div>').appendTo($('.mCSB_container')).addClass('new');
+    setDate();
+    updateScrollbar();
+    i++;
+  }, 1000 + (Math.random() * 20) * 100);
+
 }
